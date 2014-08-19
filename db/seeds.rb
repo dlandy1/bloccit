5842898cdf35d4a6de0b9554b50d79a0c474f437
vagrant <vagrant@rails-dev-box>
2014-08-19 22:27:27 +0000

    Post Details by User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 require 'faker'
- 
+ # Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+
+users = User.all
  # Create Posts
  50.times do
    Post.create(
+    user:    users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -16,7 +28,13 @@ require 'faker'
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+ User.first.update_attributes(
+  email: 'dlandy1@babson.edu',
+  password: 'fithop12',
+)
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
