fe402c94c24c566301a92594663a78fcffc18722
vagrant <vagrant@rails-dev-box>
2014-09-06 19:31:34 +0000

    create-post-with-vote-same-time-save

--- app/models/post.rb
+++ app/models/post.rb
@@ -42,6 +42,14 @@ class Post < ActiveRecord::Base
       user.votes.create(value: 1, post: self)
     end
 
+    def save_with_initial_vote
+      ActiveRecord::Base.transaction do 
+        self.save 
+        self.create_vote
+      end
+    end
+
+
     private   
 
     def render_as_markdown(text)
